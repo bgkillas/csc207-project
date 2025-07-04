6833bdec31207ddb5550ee87fb4b886954227fc6
bgkillas <walhelm@proton.me>
2025-07-04 00:24:30 -0400

    add pom.xml and test example

---
+++ src/test/java/temp/Temp.java
@@ -0,0 +1,12 @@
+package temp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Temp {
+    @Test
+    public void test() {
+        assertEquals(1 + 1, 2);
+    }
+}
