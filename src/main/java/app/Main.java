b570bc2ce1e1ead3cda3c96773934f9f88859817
bgkillas <walhelm@proton.me>
2025-07-04 09:08:55 -0400

    add build action

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,4 @@
-package java.app;
+package app;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
--- src/test/java/temp/TempTest.java
+++ src/test/java/temp/TempTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /** A simple test example. */
-public class Temp {
+public class TempTest {
     @Test
     public void test() {
         assertEquals(1 + 1, 2);
