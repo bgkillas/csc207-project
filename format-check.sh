d4e6b8bb8cadf7b560b5ac481060f79b323a9787
bgkillas <walhelm@proton.me>
2025-07-08 22:20:50 -0400

    add basic login panel

--- format-check.sh
+++ format-check.sh
@@ -1,1 +1,2 @@
-java -jar format.jar -a --set-exit-if-changed -n src/*/*/*/*
+#java -jar format.jar -a --set-exit-if-changed --skip-sorting-imports -n src/*/*/*/*
+find . -name "*.java" -print0 | xargs -0 java -jar format.jar -a --set-exit-if-changed --skip-sorting-imports
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,13 +1,23 @@
 package app;
 
+import account.login.LoginManager;
+import account.login.LoginManagerMemory;
+import java.security.NoSuchAlgorithmException;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import view.LoginView;
 
 /** Main executable class. */
 public class Main {
     /** Main executable point. */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         final JFrame application = new JFrame("app");
+        final JPanel views = new JPanel();
+        final LoginManager login_manager = new LoginManagerMemory();
+        final JPanel login = LoginView.create(login_manager);
+        views.add(login);
+        application.add(views);
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         application.pack();
         application.setVisible(true);
