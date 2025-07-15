5265e68c8d2b2af47d1ce62250cbff471f1ba3fe
bgkillas <walhelm@proton.me>
2025-07-15 18:41:50 -0400

    launch url directly if applicable

--- src/main/java/spotify/SpotifyInterface.java
+++ src/main/java/spotify/SpotifyInterface.java
@@ -7,11 +7,12 @@ public interface SpotifyInterface {
     /** Pulls top artists and genres in the user profile to the class. */
     void pullTopArtistsAndGenres();
 
-    /** gets top artists in the user profile from the class.
-    * limited to at most 10 unique artists */
+    /** gets top artists in the user profile from the class. limited to at most 10 unique artists */
     List<String> getTopArtists();
 
-    /** gets top genres in the user profile from the class.
-    * arbitrary amount of potentially duplicated unsorted genres*/
+    /**
+     * gets top genres in the user profile from the class. arbitrary amount of potentially
+     * duplicated unsorted genres
+     */
     List<String> getGenres();
 }
