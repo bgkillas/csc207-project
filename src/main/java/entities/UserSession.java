94ee23dd57a1d5c9c704e770b2ee61866df4bc07
JamieXW <jamiewang27@gmail.com>
2025-07-14 20:35:17 -0400

    implemented matchService class for Match users use case and javadocs for other files

--- src/main/java/entities/UserSession.java
+++ src/main/java/entities/UserSession.java
@@ -1,14 +1,23 @@
+
 package entities;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a session for a logged-in user.
+ */
 public class UserSession {
     private final User user;
     private final List<User> incomingMatches;
     private final List<User> outgoingMatches;
     private final List<Match> matches;
 
+    /**
+     * Constructs a UserSession for the given user.
+     *
+     * @param user the current user
+     */
     public UserSession(User user) {
         this.user = user;
         this.incomingMatches = new ArrayList<>();
@@ -16,30 +25,66 @@ public class UserSession {
         this.matches = new ArrayList<>();
     }
 
+    /**
+     * Returns the current user for this session.
+     *
+     * @return the current user
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Returns the list of users who have sent match requests to the current user.
+     *
+     * @return the list of incoming match users
+     */
     public List<User> getIncomingMatches() {
         return incomingMatches;
     }
 
+    /**
+     * Returns the list of users to whom the current user has sent match requests.
+     *
+     * @return the list of outgoing match users
+     */
     public List<User> getOutgoingMatches() {
         return outgoingMatches;
     }
 
+
+    /**
+     * Returns the list of matches for the current user.
+     *
+     * @return the list of matches
+     */
     public List<Match> getMatches() {
         return matches;
     }
 
+    /**
+     * Adds a user to the list of incoming match requests.
+     *
+     * @param other the user who sent a match request
+     */
     public void addIncomingMatch(User other) {
         incomingMatches.add(other);
     }
 
+    /**
+     * Adds a user to the list of outgoing match requests.
+     *
+     * @param other the user to whom a match request was sent
+     */
     public void addOutgoingMatch(User other) {
         outgoingMatches.add(other);
     }
 
+    /**
+     * Adds a match to the list of matches for the current user.
+     *
+     * @param match the match to add
+     */
     public void addMatch(Match match) {
         matches.add(match);
     }
