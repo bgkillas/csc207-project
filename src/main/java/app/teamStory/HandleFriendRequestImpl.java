1bad0a08c62e2eaf2e21caf9a25d0d9992bfc225
Your Name <you@example.com>
2025-07-22 14:17:48 -0400

    injected DAOs into related usecase and updated UserSession

--- src/main/java/app/teamStory/HandleFriendRequestImpl.java
+++ src/main/java/app/teamStory/HandleFriendRequestImpl.java
@@ -1,30 +1,63 @@
 package app.teamStory;
 
+import data_access.MatchDataAccessInterface;
 import entities.User;
 import entities.UserSession;
 import usecase.teamStory.AddFriendsList;
 import usecase.teamStory.HandleFriendRequest;
 
 public class HandleFriendRequestImpl implements HandleFriendRequest {
+    private final MatchDataAccessInterface matchDAO;
+
+    /**
+     * Creates use case for handling friend request.
+     * @param matchDAO data access object for matches.
+     */
+    public HandleFriendRequestImpl(MatchDataAccessInterface matchDAO) {
+        this.matchDAO = matchDAO;
+    }
+
+    /**
+     * Sends friend request to another user.
+     * @param userSession the current session.
+     * @param toUser the user the request is being sent to.
+     */
     @Override
     public void sendFriendRequest(UserSession userSession, User toUser) {
-        userSession.getOutgoingMatches().add(toUser);
-        System.out.println("Friend request sent to " + toUser.getName());
+        User currentUser = userSession.getUser();
+
+        // add user to outgoing
+        matchDAO.addOutgoingFriendRequest(userSession.getUser(), toUser);    // Repo updates
+        userSession.getOutgoingMatches().add(toUser);                        // Session updates
+        // add user to incoming
+        matchDAO.addIncomingFriendRequest(toUser, currentUser);              // Repo updates
+        userSession.getIncomingMatches().add(currentUser);                   // Session updates
     }
 
     @Override
     public void acceptFriendRequest(UserSession userSession, User fromUser) {
-        // remove user from incoming
-        userSession.getIncomingMatches().remove(fromUser);
+        User currentUser = userSession.getUser();
 
-        AddFriendsList addFriendsListImp = new AddFriendsListImpl();
-        addFriendsListImp.addFriend(userSession.getUser(), fromUser);
-        System.out.println("Friend request accepted from " + fromUser.getName());
+        // remove user from incoming
+        matchDAO.getIncomingFriendRequest(currentUser).remove(fromUser);    // Repo updates
+        userSession.getIncomingMatches().remove(fromUser);                  // Session updates
+
+        // Remove from fromUser's outgoing
+        matchDAO.getOutgoingFriendRequest(fromUser).remove(currentUser);
+
+        AddFriendsList addFriendsListImpl = new AddFriendsListImpl();
+        addFriendsListImpl.addFriend(userSession.getUser(), fromUser);
     }
 
     @Override
     public void declineFriendRequest(UserSession userSession, User fromUser) {
-        userSession.getIncomingMatches().remove(fromUser);
-        System.out.println("Friend request declined to " + fromUser.getName());
+        User currentUser = userSession.getUser();
+
+        // remove user from incoming
+        matchDAO.getIncomingFriendRequest(currentUser).remove(fromUser);    // Repo updates
+        userSession.getIncomingMatches().remove(fromUser);                  // Session updates
+
+        // Remove from fromUser's outgoing
+        matchDAO.getOutgoingFriendRequest(fromUser).remove(currentUser);
     }
 }
